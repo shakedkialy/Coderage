e7b57f95e4e2dad4183ef3eb1dc477a410040de9
shaked.kialy <shaked.kialy@huji.mail.ac.il>
2021-06-22 11:53:44 +0300

    fix imports

--- Main/HTML/__init__.py
+++
@@ -1,4 +0,0 @@
-from .HTML import *
-from .html_files import *
-from .HTML_TEMPLATES import *
-from .JS_TEMPLATES import *
--- Main/__init__.py
+++
@@ -1,3 +0,0 @@
-from .Parser import *
-from .HTML import *
-from .DB import *
